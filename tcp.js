1b2bda9d6eaba67e38b1dd43bcec36042a8cd5d8
buobao <lebron_kine_james@outlook.com>
2018-05-15 14:22:59 +0800

    push from my computer

---
+++ tcp.js
@@ -0,0 +1,8 @@
+var net = require("net");
+var server = net.createServer(function(socket){
+	console.log('someone connects');
+});
+
+server.listen(18001, function(){
+	console.log('server is listening');
+});
